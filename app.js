5aa135280d3aaec26b43805b9a3f097a78e3b084
khleoshih <b02901086@ntu.edu.tw>
2017-03-21 02:04:21 +0800

    basic function

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
-var app = require('express')();
+const express = require("express");
+const app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users=[];
 
-app.get('/', function(req, res){
+/*app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-});
+});*/
+
+app.use(express.static("../chatroom")); 
 
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
+    let off_name_id = users.indexOf(socket.name);
+    users.splice(off_name_id,1);
+    socket.broadcast.emit('offline', socket.name);
     console.log('user disconnected');
   });
   socket.on('username', function(name){
     socket.name = name;
     socket.join(name);
+    io.sockets.in(name).emit('allusers', users);
+    users.push(name);
+    io.emit('online', socket.name);
   });
   socket.on('chat message', function(msg){
-    io.sockets.in(msg.obj).emit('chat', socket.name + ': ' + msg.msg);
+    let m = {
+      head: socket.name,
+      message: msg.msg,
+      to: msg.obj
+    }
+    if (msg.obj === "public"){
+      io.emit('chat', m);
+    }
+    else{
+      io.sockets.in(msg.obj).emit('chat', m);
+    }
     console.log('message: ' + msg + ' from ' + socket.name + ' to ' + msg.obj);
   });
 });
