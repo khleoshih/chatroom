122e495e970e4598ea1034cac0bc07eb26dfa122
khleoshih <b02901086@ntu.edu.tw>
2017-03-24 22:05:11 +0800

    final updaate

--- app.js
+++ app.js
@@ -4,26 +4,23 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var users=[];
 
-/*app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});*/
-
 app.use(express.static("../chatroom")); 
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.emit('allusers', users);
   socket.on('disconnect', function(){
+    console.log('user disconnected: ' + socket.name);
     let off_name_id = users.indexOf(socket.name);
     users.splice(off_name_id,1);
     socket.broadcast.emit('offline', socket.name);
-    console.log('user disconnected');
   });
   socket.on('username', function(name){
     socket.name = name;
     socket.join(name);
-    io.sockets.in(name).emit('allusers', users);
     users.push(name);
     io.emit('online', socket.name);
+    console.log("online setting done: " + name);
   });
   socket.on('chat message', function(msg){
     let m = {
@@ -37,10 +34,9 @@ io.on('connection', function(socket){
     else{
       io.sockets.in(msg.obj).emit('chat', m);
     }
-    console.log('message: ' + msg + ' from ' + socket.name + ' to ' + msg.obj);
+    console.log('message: ' + msg.msg + ' from ' + socket.name + ' to ' + msg.obj);
   });
 });
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(process.env.PORT || 3000, function(){
+	console.log('listening on *:3000');
 });
