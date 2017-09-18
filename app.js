8f171206822472dad5ec34a3a2e1acd7beb9a58e
khleoshih <leoshih1012@gmail.com>
2017-09-18 21:13:36 +0800

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var users=[];
+var users=[]; 
 
 app.use(express.static("../chatroom")); 
 
